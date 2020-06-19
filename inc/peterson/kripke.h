055a476ce9246baf240a29082b084443dcb673b8
Majeux <manblankestijn@gmail.com>
2020-06-19 23:17:23 +0200

    slightly generalized mutex/starvation checks

--- inc/peterson/kripke.h
+++ inc/peterson/kripke.h
@@ -3,89 +3,98 @@
 
 #include "peterson/iterator.h"
 
-class PetersonKripke: public spot::kripke
-{
+class PetersonKripke: public spot::kripke {
     private:
-        const size_t _N;
+        const proc _N;
         bdd* crit; //is process i < _N in critical section (for verifying mutex)
         bdd* wait; //is process i < _N waiting to access critical section (for verifying non-starvation)
 
     public:
-    PetersonKripke(size_t n, const spot::bdd_dict_ptr& d)
-        : spot::kripke(d), _N(n)
-    {
-        crit = new bdd[_N];
-        wait = new bdd[_N];
+        PetersonKripke(size_t n, const spot::bdd_dict_ptr& d)
+            : spot::kripke(d), _N(n)
+        {
+            assert(_N > 0);
+            crit = new bdd[_N];
+            wait = new bdd[_N];
 
-        for(size_t i = 0; i < _N; i++) {
-            std::string critical = "crit"; critical += '0' + i;
-            std::string waiting  = "wait"; waiting  += '0' + i;
-
-            crit[i] = bdd_ithvar( register_ap(critical) );
-            wait[i] = bdd_ithvar( register_ap(waiting) );
-        }
-    }
-
-    ~PetersonKripke() {
-        delete[] crit;
-        delete[] wait;
-    }
-
-    PetersonState* get_init_state() const override
-    {
-        return new PetersonState(_N);
-    }
-
-    //recycling prevents delete/new overhead
-    PetersonIterator* succ_iter(const spot::state* s) const override {
-        const PetersonState* state = static_cast<const PetersonState*>(s);
-        bdd condition = state_condition(state);
-
-        if(iter_cache_) { //reuse old iterator if available
-            PetersonIterator* i = static_cast<PetersonIterator*>(iter_cache_);
-            iter_cache_ = nullptr;
-            i->recycle(state, condition);
-
-            return i;
+            for(size_t i = 0; i < (size_t)_N; i++) {
+                crit[i] = bdd_ithvar( register_ap(critical_string(i)) );
+                wait[i] = bdd_ithvar( register_ap(waiting_string(i)) );
+            }
         }
 
-        return new PetersonIterator(state, condition);
-    }
-
-    bdd state_condition(const spot::state* s) const override
-    {
-        const PetersonState* state = static_cast<const PetersonState*>(s);
-
-        const std::vector<proc>* pc  = state->getPC();
-        const std::vector<proc>* lvl = state->getLVL();
-
-        bdd crit_condition = (*pc)[0] == 4 ? crit[0] : !crit[0];
-        bdd wait_condition = (*lvl)[0] > -1 && (*lvl)[0] < 4 ? wait[0] : !wait[0];
-
-        for(size_t i = 1; i < _N; i++) {
-            crit_condition &= (*pc)[i] == 4 ? crit[i] : !crit[i];
-            wait_condition &= (*lvl)[i] > -1 && (*lvl)[i] < 4 ? wait[i] : !wait[i];
+        ~PetersonKripke() {
+            delete[] crit;
+            delete[] wait;
         }
 
-        return crit_condition & wait_condition;
-    }
+        PetersonState* get_init_state() const override
+        {
+            return new PetersonState(_N);
+        }
 
-    std::string format_state(const spot::state* s) const override
-    {
-        auto state = static_cast<const PetersonState*>(s);
-        std::ostringstream out;
-        out << "pc = [ ";
-        for(auto i : *(state->getPC())) out << (long int)i << ", ";
-        out << " ]" << std::endl
-            << "level = [ ";
-        for(auto i : *(state->getLVL())) out << (long int)i << ", ";
-        out << " ] "
-            << "last_to_enter = [ ";
-        for(auto i : *(state->getLTE())) out << (long unsigned)i << ", ";
-        out << " ]" << std::endl;
+        //recycling prevents delete/new overhead
+        PetersonIterator* succ_iter(const spot::state* s) const override {
+            const PetersonState* state = static_cast<const PetersonState*>(s);
+            bdd condition = state_condition(state);
 
-        return out.str();
-    }
+            if(iter_cache_) { //reuse old iterator if available
+                PetersonIterator* i = static_cast<PetersonIterator*>(iter_cache_);
+                iter_cache_ = nullptr;
+                i->recycle(state, condition);
+
+                return i;
+            }
+
+            return new PetersonIterator(state, condition);
+        }
+
+        bdd state_condition(const spot::state* s) const override
+        {
+            const PetersonState* state = static_cast<const PetersonState*>(s);
+
+            const std::vector<proc>* pc  = state->getPC();
+            const std::vector<proc>* lvl = state->getLVL();
+
+            bdd crit_condition = (*pc)[0] == 4 ? crit[0] : !crit[0];
+            bdd wait_condition = (*lvl)[0] > -1 && (*lvl)[0] < 4 ? wait[0] : !wait[0];
+
+            for(size_t i = 1; i < (size_t)_N; i++) {
+                crit_condition &= (*pc)[i] == 4 ? crit[i] : !crit[i];
+                wait_condition &= (*lvl)[i] > -1 && (*lvl)[i] < 4 ? wait[i] : !wait[i];
+            }
+
+            return crit_condition & wait_condition;
+        }
+
+        std::string format_state(const spot::state* s) const override
+        {
+            auto state = static_cast<const PetersonState*>(s);
+            std::ostringstream out;
+            out << "pc = [ ";
+            for(auto i : *(state->getPC())) out << (long int)i << ", ";
+            out << " ]" << std::endl
+                << "level = [ ";
+            for(auto i : *(state->getLVL())) out << (long int)i << ", ";
+            out << " ] "
+                << "last_to_enter = [ ";
+            for(auto i : *(state->getLTE())) out << (long unsigned)i << ", ";
+            out << " ]" << std::endl;
+
+            return out.str();
+        }
+
+        std::string critical_string(proc i) const {
+            std::string critical = "crit";
+            critical += '0' + i;
+            return critical;
+        }
+
+        std::string waiting_string(proc i) const {
+            std::string waiting = "wait";
+            waiting += '0' + i;
+            return waiting;
+        }
 };
 
 #endif
--- main/main.cc
+++ main/main.cc
@@ -9,6 +9,40 @@
 #include "peterson/kripke.h"
 
 int main() {
+    spot::bdd_dict_ptr dict = spot::make_bdd_dict();
+    const proc N = 4;
+    auto pk = std::make_shared<PetersonKripke>(N, dict);
+
+    auto verify_starvation = [=] () {
+        std::ostringstream formula;
+
+        for(proc i = 0; i < N; i++) { //starvation free: any process tthat starts waiting, gets access
+            if(i > 0)
+                formula << " && ";
+
+            formula << pk->waiting_string(i) << " -> F("
+                    << pk->critical_string(i) << " && !" << pk->waiting_string(i) << ")";
+        }
+        return formula.str();
+    };
+
+    auto verify_mutex = [=] () {
+        std::ostringstream formula;
+        std::ostringstream formula2;
+
+        formula << "G( (";
+        for(size_t i = 0; i < N; i++) { //starvation free: any process tthat starts waiting, gets access
+            if(i > 0) {
+                formula  << " && ";
+                formula2 << " xor ";
+            }
+
+            formula  << '!' << pk->critical_string(i);
+            formula2 << pk->critical_string(i);
+        }
+        formula << ") || (" << formula2.str() << ") )";
+        return formula.str();
+    };
 
     // Checker checker;
     // std::cerr << "Printing test kripke" << std::endl;
@@ -23,21 +57,30 @@ int main() {
     //     std::cerr << "-- READ ERROR" << std::endl;
     //
     // std::cerr << std::endl << "-- done" << std::endl;
-    spot::bdd_dict_ptr dict = spot::make_bdd_dict();
-
-    std::string formula = "wait0 -> F(crit0 && !wait0) && wait1 -> F(crit1 && !wait1) "; //starvation free: any process tthat starts waiting, gets access
 
     // std::string formula = "G( (!crit0 && !crit1) || crit0 xor crit1)"; //mutex
+    std::string f_string = verify_mutex();
 
-    spot::parsed_formula pf = spot::parse_infix_psl(formula);
+    spot::parsed_formula pf = spot::parse_infix_psl(f_string);
     spot::formula f = spot::formula::Not(pf.f);
     spot::twa_graph_ptr f_auto = spot::translator(dict).run(f);
 
-    auto pk = std::make_shared<PetersonKripke>(2, dict);
     if(auto run = pk->intersecting_run(f_auto))
-        std::cout << formula << " violated by: \n" << *run << std::endl;
+        std::cout << f_string << " violated by: \n" << *run << std::endl << std::endl;
     else
-        std::cout << formula << " verified" << std::endl;
+        std::cout << "verified:\n" << f_string << std::endl <<  std::endl;
+
+    f_string = verify_starvation();
+
+    pf = spot::parse_infix_psl(f_string);
+    f = spot::formula::Not(pf.f);
+    f_auto = spot::translator(dict).run(f);
+
+    if(auto run = pk->intersecting_run(f_auto))
+        std::cout << f_string << " violated by: \n __________________"
+                  << *run << std::endl << std::endl;
+    else
+        std::cout << "verified:\n"<< f_string << std::endl << std::endl;
 
     // spot::print_dot(std::cout, pk);
 
