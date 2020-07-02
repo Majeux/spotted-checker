1337b11ad750f660d8a31ab80e480ec1d859745d
Majeux <manblankestijn@gmail.com>
2020-07-02 14:53:30 +0200

    correct (but lengthy) starvation condition

--- main/main.cc
+++ main/main.cc
@@ -8,12 +8,14 @@
 
 int main() {
     Checker checker;
-    spot::bdd_dict_ptr dict = spot::make_bdd_dict();
+
     const proc N = 3;
-    std::cout << "Peterson's with N = " << N << "___________________" << std::endl;
+    spot::bdd_dict_ptr dict = spot::make_bdd_dict();
+    std::cout << "Peterson's with N = " << (long)N << "\n___________________" << std::endl;
     // auto pk = std::make_shared<PetersonKripke>(N, dict);
     auto pk = std::make_shared<MyKripke>(N, dict);
     // spot::print_dot(std::cout, pk);
+
     auto starvation = [=] (std::function<std::string(proc)> crit, std::function<std::string(proc)> wait) {
         std::ostringstream formula;
         formula << "G( ";
@@ -66,33 +68,36 @@ int main() {
     std::function<std::string(proc)> critical = std::bind(pk->critical_string, std::placeholders::_1);
     std::function<std::string(proc)> waiting  = std::bind(pk->waiting_string, std::placeholders::_1);
 
-    std::cout << "MUTEX" << std::endl;
-    Checker::spotVerify( pk, mutex(critical) );
+    // std::cout << "MUTEX" << std::endl;
+    // Checker::spotVerify( pk, mutex(critical) );
+    //
+    // std::cout << "STARVE" << std::endl;
+    // Checker::spotVerify( pk, starvation(critical, waiting) );
+    //
+    // std::cout << "BOUNDED_WAITING" << std::endl;
+    // Checker::spotVerify( pk, bounded_waiting(critical, waiting) );
+    //
+    // std::cout << "DOUBLE CRIT" << std::endl;
+    // Checker::spotVerify( pk, "F(crit0 && crit 1)");
+    //
+    // std::cout << "------------------------" << std::endl
+    //           << "MY TURN" << std::endl
+    //           << "------------------------" << std::endl;
 
-    std::cout << "STARVE" << std::endl;
-    Checker::spotVerify( pk, starvation(critical, waiting) );
+    // std::cout << "MUTEX" << std::endl;
+    // Checker::myVerify( pk, mutex(critical) );
+    //
+    // std::cout << "STARVE" << std::endl;
+    // Checker::myVerify( pk, starvation(critical, waiting) );
+    Checker::myVerify( pk, "G(  (wait0 -> ( F(crit0 && !wait0) || (F(wait1 && F(crit1 && !wait1))) || (F(wait2 && F(crit2 && !wait2))) ) )"); //" && (wait1 -> ( F(crit0 && !wait0) || F(crit1 && !wait1) || F(crit2 && !wait2) ) ) && (wait2 -> ( F(crit0 && !wait0) || F(crit1 && !wait1) || F(crit2 && !wait2) ) ) )" );
 
-    std::cout << "BOUNDED_WAITING" << std::endl;
-    Checker::spotVerify( pk, bounded_waiting(critical, waiting) );
-
-    std::cout << "DOUBLE CRIT" << std::endl;
-    Checker::spotVerify( pk, "F(crit0 && crit 1)");
-
-    std::cout << "------------------------" << std::endl
-              << "MY TURN" << std::endl
-              << "------------------------" << std::endl;
-
-    std::cout << "MUTEX" << std::endl;
-    Checker::myVerify( pk, mutex(critical) );
-
-    std::cout << "STARVE" << std::endl;
-    Checker::myVerify( pk, starvation(critical, waiting) );
-
-    std::cout << "BOUNDED_WAITING" << std::endl;
-    Checker::myVerify( pk, bounded_waiting(critical, waiting) );
-
-    std::cout << "DOUBLE CRIT" << std::endl;
-    Checker::myVerify( pk, "F(crit0 && crit 1)");
+    // Checker::myVerify( pk, "G( wait0 -> (wait0 W crit0) && wait1 -> (wait1 W crit1) && wait2 -> (wait2 W crit2)) )" );
+    //
+    // std::cout << "BOUNDED_WAITING" << std::endl;
+    // Checker::myVerify( pk, bounded_waiting(critical, waiting) );
+    //
+    // std::cout << "DOUBLE CRIT" << std::endl;
+    // Checker::myVerify( pk, "F(crit0 && crit 1)");
 
     // auto traffic = Checker::explicit_traffic();
     // auto traffic_buchi = Checker::defineTrafficBuchi(traffic);
